4b14c815ed832c6ae644cd5ef501a30ae898b2bf
kunfei <gekunfei@live.com>
2026-08-19 12:23:27 +0800

    优化

--- src/main/kotlin/com/github/gedoor/jarpackage/ui/ActionOpenFile.kt
+++ src/main/kotlin/com/github/gedoor/jarpackage/ui/ActionOpenFile.kt
@@ -1,10 +1,10 @@
 package com.github.gedoor.jarpackage.ui
 
 import com.github.gedoor.jarpackage.util.Constants
-import com.intellij.ide.BrowserUtil
 import com.intellij.ide.actions.RevealFileAction
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import java.awt.Desktop
 import java.nio.file.Files
 import java.nio.file.Path
 
@@ -15,7 +15,7 @@ class ActionOpenFile(val filePath: Path) : AnAction(Constants.actionNameOpen) {
 
     override fun actionPerformed(e: AnActionEvent) {
         if (!Files.isDirectory(filePath)) {
-            BrowserUtil.browse(filePath.toUri())
+            Desktop.getDesktop().open(filePath.toFile())
         } else {
             RevealFileAction.openDirectory(filePath.toFile())
         }
